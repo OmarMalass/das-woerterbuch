a46177ebeb49988484c6609391ff735b6ac9f8bf
OmarMalass <Omar1Malass@gmail.com>
2021-02-11 16:00:51 +0200

    Initial commit

---
+++ src/main/kotlin/com/dwb/vokabularium/VokabulariumApplication.kt
@@ -0,0 +1,12 @@
+package com.dwb.vokabularium
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class VokabulariumApplication
+
+fun main(args: Array<String>) {
+    runApplication<VokabulariumApplication>(*args)
+}
